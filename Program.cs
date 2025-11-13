7a8ca9d15ebbfb86e791f6caac897b6dbf00134b
Antonio <antoniorees74@gmail.com>
2025-11-13 12:20:28 +0000

    feat(docs): implement and configure Swagger UI  - Integrates Swashbuckle to provide live, interactive API documentation.  - Adds AddEndpointsApiExplorer to enable API discovery in a Minimal API project.  - Configures AddSwaggerGen to include XML comments for richer documentation.  - - Enriches Minimal API endpoints with .Produces, .WithSummary, and .WithDescription      for complete and accurate metadata.

--- Program.cs
+++ Program.cs
@@ -3,24 +3,37 @@ using JobTracker.Api.Services;
 using JobTracker.Api.Endpoints;
 using JobTracker.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IEmployersRepository, MySqlEmployersRepository>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 0));
 
 builder.Services.AddDbContext<JobTrackerDbContext>(options =>
 {
-    
+
     options.UseMySql(connectionString, serverVersion);
 
 }, ServiceLifetime.Scoped);
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var XmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, XmlFilename);
+
+    options.IncludeXmlComments(xmlPath);
+});
+
 var app = builder.Build();
 
 app.MapEmployersEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
